a667b4c935fdcc4b46986ec4668c410ba64372a5
Emus88 <e.musrepov@gmail.com>
2022-01-27 22:12:45 +0700

    test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,33 +1,39 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/EMus88/go-musthave-shortener-tpl/configs"
 	"github.com/EMus88/go-musthave-shortener-tpl/internal/app/service"
 	"github.com/EMus88/go-musthave-shortener-tpl/internal/handler"
 	"github.com/EMus88/go-musthave-shortener-tpl/internal/repository"
-	"github.com/EMus88/go-musthave-shortener-tpl/internal/repository/models/file"
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	var model file.Model
-
 	config := configs.NewConfig()
-	r := repository.NewStorage()
-	s := service.NewService(r, &model, config)
-	h := handler.NewHandler(s)
+	ctx := context.Background()
+	db, err := repository.NewDBClient(ctx, config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//load data from file
-	s.LoadFromFile()
+	repository.Migration(config)
+
+	r := repository.NewStorage(db)
+	s := service.NewService(r, config)
+	h := handler.NewHandler(s)
 
 	//start server
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	router.GET("/:id", h.HandlerGet)
-	router.POST("/", h.HandlerPostText)
-	router.POST("/api/shorten", h.HandlerPostJSON)
+	router.GET("/:id", h.HandlerURLRelocation)
+	router.POST("/", h.HandlerPost)
+	router.POST("/api/shorten", h.HandlerPost)
 	router.NoRoute(func(c *gin.Context) { c.String(http.StatusBadRequest, "Not allowed requset") })
 
 	router.Run(config.ServerAdress)
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,32 +1,152 @@
 package handler
 
 import (
-	"fmt"
+	"compress/gzip"
+	"errors"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/EMus88/go-musthave-shortener-tpl/internal/app/service"
+	"github.com/asaskevich/govalidator"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
-	service *service.Service
+	service   *service.Service
+	sessionID string
 }
 type Request struct {
-	LongURL string `json:"url"`
+	LongURL string `json:"url"  binding:"required"`
+	body    []byte
 }
-type Result struct {
-	Result string `json:"result"`
+type Response struct {
+	ShortURL string `json:"result"`
 }
 
 func NewHandler(service *service.Service) *Handler {
 	return &Handler{service: service}
 }
 
+//================================================================
+func isEncodingSupport(c *gin.Context) bool {
+	//if the client supports compression
+	if strings.Contains(c.GetHeader("Accept-Encoding"), "qweqwe") {
+		return true
+		//if the client doesn't support compression
+	} else {
+		return false
+	}
+}
+
 //=================================================================
-func (h *Handler) HandlerGet(c *gin.Context) {
+func parseRequest(c *gin.Context) (*Request, error) {
+	var request Request
+
+	switch c.Request.Header.Get("content-type") {
+	case "application/json":
+		if err := c.ShouldBindJSON(&request); err != nil {
+			return nil, err
+		}
+		if ok := govalidator.IsURL(string(request.LongURL)); !ok {
+			return nil, errors.New("error")
+		}
+		request.body = []byte(request.LongURL)
+
+	case "text/plain":
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			return nil, err
+		}
+		if len(body) < 1 {
+			return nil, errors.New("error")
+		}
+		if ok := govalidator.IsURL(string(body)); !ok {
+			return nil, errors.New("error")
+		}
+		request.body = body
+
+	case "application/x-gzip":
+		if strings.Contains(c.GetHeader("Content-Encoding"), "gzip") {
+			reader, err := gzip.NewReader(c.Request.Body)
+			if err != nil {
+				return nil, err
+			}
+			defer reader.Close()
+			body, err := ioutil.ReadAll(reader)
+			if err != nil {
+				return nil, err
+			}
+			request.body = body
+		}
+	default:
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			return nil, err
+		}
+		request.body = body
+	}
+	return &request, nil
+}
+
+//================================================================
+func renderResponse(c *gin.Context, response *Response) {
+
+	if isEncodingSupport(c) {
+		c.Status(http.StatusCreated)
+		gz := gzip.NewWriter(c.Writer)
+		defer gz.Close()
+		gz.Write([]byte(response.ShortURL))
+		c.Writer.Header().Set("Content-Encoding", "gzip")
+		c.Writer.Header().Set("Content-Type", "application/x-gzip")
+
+	} else {
+		switch c.Request.Header.Get("content-type") {
+		case "application/json":
+			c.JSON(http.StatusCreated, response.ShortURL)
+		case "text/plain":
+			c.String(http.StatusCreated, response.ShortURL)
+		default:
+			c.String(http.StatusCreated, response.ShortURL)
+		}
+	}
+}
+
+//==================================================
+
+func AuthMiddleware(h *Handler) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		sessionID, err := c.Cookie("session")
+
+		//validation session value
+		//var id int = h.service.Repository.GetCookieID("sdfsdfsdf")
+		//fmt.Println(id)
+
+		if err != nil {
+			if !errors.Is(err, http.ErrNoCookie) {
+				log.Fatal(err)
+			}
+			_, encID, err := h.service.Auth.CreateSissionID()
+			if err != nil {
+				log.Fatal(err)
+			}
+			sessionID = encID
+		}
+
+		c.SetCookie("session", sessionID, 3600, "", "localhost", false, true)
+
+		c.Next()
+	}
+}
+
+//=================================================================
+func (h *Handler) HandlerURLRelocation(c *gin.Context) {
+	log.Println("GET hanler")
 	id := c.Param("id")
 	longURL, err := h.service.GetURL(id)
+
 	if err != nil {
 		c.String(http.StatusBadRequest, err.Error())
 		return
@@ -35,38 +155,28 @@ func (h *Handler) HandlerGet(c *gin.Context) {
 	c.Header("Location", longURL)
 }
 
-//==================================================================
-func (h *Handler) HandlerPostText(c *gin.Context) {
-	body, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil || len(body) == 0 {
-		c.String(http.StatusBadRequest, "Not allowed request")
-		return
+//=================================================================
+func (h *Handler) HandlerPingDB(c *gin.Context) {
+	if err := h.service.Repository.PingDB(); err != nil {
+		c.String(http.StatusInternalServerError, "DB connection is not available")
 	}
-	id, err := h.service.SaveURL(string(body))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
-	}
-	c.String(http.StatusCreated, h.service.Config.BaseURL+"/"+id)
+	c.String(http.StatusOK, "DB connection succes")
 }
 
-//===================================================================
-func (h *Handler) HandlerPostJSON(c *gin.Context) {
-	var request Request
-	if err := c.ShouldBindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Not allowed request"})
-		return
-	}
-	if request.LongURL == "" || c.GetHeader("content-type") != "application/json" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Not allowed request"})
+//==================================================================
+func (h *Handler) HandlerPost(c *gin.Context) {
+	request, err := parseRequest(c)
+	if err != nil {
+		c.String(http.StatusBadRequest, "error: Not Allowd request")
 		return
 	}
 
-	id, err := h.service.SaveURL(request.LongURL)
+	shortURL, err := h.service.SaveURL(string(request.body), h.sessionID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		c.String(http.StatusInternalServerError, "error: Internal error")
 	}
-	shortURL := fmt.Sprint(h.service.Config.BaseURL, "/", id)
-	var result Result
-	result.Result = shortURL
-	c.JSON(http.StatusCreated, result)
+	//write result
+	var response Response
+	response.ShortURL = shortURL
+	renderResponse(c, &response)
 }
